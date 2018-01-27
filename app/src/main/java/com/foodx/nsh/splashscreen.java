6a6085ee58225b9c4e0e608885f0b9f21c907d78
ThisIsNSH <hadanis.singh@gmail.com>
2018-01-27 15:58:33 +0530

    splash screen

---
+++ app/src/main/java/com/foodx/nsh/splashscreen.java
@@ -0,0 +1,32 @@
+package com.foodx.nsh;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splashscreen extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+
+                Intent i = new Intent(splashscreen.this, MainActivity.class);
+                startActivity(i);
+
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
