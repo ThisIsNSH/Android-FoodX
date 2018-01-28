6ce720cf50cbcd733802284651ec45cf0bf6d104
ThisIsNSH <hadanis.singh@gmail.com>
2018-01-28 19:22:04 +0530

    address

---
+++ app/src/main/java/com/foodx/nsh/address.java
@@ -0,0 +1,13 @@
+package com.foodx.nsh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class address extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_address);
+    }
+}
