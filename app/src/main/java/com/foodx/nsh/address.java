c2243c1e0dbffc67cacefc9d91c8ed2466e501dc
ThisIsNSH <hadanis.singh@gmail.com>
2018-02-15 18:48:09 +0530

    order

--- app/src/main/java/com/foodx/nsh/address.java
+++ app/src/main/java/com/foodx/nsh/address.java
@@ -26,130 +26,20 @@ public class address extends AppCompatActivity {
     final public String order_temp=order;
     private DatabaseReference mDatabase;
 
-    public String address_temp1="null",mobile1="null";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address);
 
-        final TextView a = findViewById(R.id.a);
-        final TextView m = findViewById(R.id.m);
 
-        final TextView old = findViewById(R.id.oldaddress);
-        final TextView oldp = findViewById(R.id.oldphone);
-        final EditText new1 = findViewById(R.id.newaddress);
-        final EditText newp = findViewById(R.id.newphone);
-        final TextView addclick = findViewById(R.id.addclick);
-
-        final TextInputLayout inputa = findViewById(R.id.input_layout_address);
-        final TextInputLayout inputm = findViewById(R.id.input_layout_phone);
-
-        final SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
-        final SharedPreferences.Editor editor = pref.edit();
-
-
-        old.setText(pref.getString("address","No Address"));
-        oldp.setText(pref.getString("phone","No Mobile Number"));
-
-
-
-        String address_temp=old.getText().toString();
-        String mobile=oldp.getText().toString();
-
-/*
-        inputa.setVisibility(View.GONE);
-    inputm.setVisibility(View.GONE);*/
-
-        /*addclick.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {*/
-/*
-                a.setVisibility(View.GONE);
-                m.setVisibility(View.GONE);
-
-                old.setVisibility(View.GONE);
-                oldp.setVisibility(View.GONE);
-                addclick.setVisibility(View.GONE);
-                inputa.setVisibility(View.VISIBLE);
-                inputm.setVisibility(View.VISIBLE);*/
-
-                new1.addTextChangedListener(new TextWatcher() {
-                    @Override
-                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                    }
-
-                    @Override
-                    public void onTextChanged(CharSequence s, int start, int before, int count) {
-                        editor.putString("address", new1.getText().toString());
-                        old.setText(new1.getText().toString());
-                        address_temp1=old.getText().toString();
-                        editor.commit();
-                    }
-
-                    @Override
-                    public void afterTextChanged(Editable s) {
-                        editor.putString("address", new1.getText().toString());
-                        old.setText(new1.getText().toString());
-                        address_temp1=old.getText().toString();
-                        editor.commit();
-
-                    }
-                });
-
-
-
-                newp.addTextChangedListener(new TextWatcher() {
-                    @Override
-                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                    }
-
-                    @Override
-                    public void onTextChanged(CharSequence s, int start, int before, int count) {
-                        editor.putString("phone", newp.getText().toString());
-                        oldp.setText(newp.getText().toString());
-                        mobile1=oldp.getText().toString();
-
-                        editor.commit();
-                    }
-
-                    @Override
-                    public void afterTextChanged(Editable s) {
-                        editor.putString("phone", newp.getText().toString());
-                        oldp.setText(newp.getText().toString());
-                        mobile1=oldp.getText().toString();
-
-                        editor.commit();
-                    }
-                });
-
-
-          /*  }
-        });*/
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
         final String address_final,mobile_final;
 
         final String total_temp= "" + (total);
-        if (mobile1=="null")
-        {
-            mobile_final=mobile;
-        }
-        else{
-            mobile_final=mobile1;
-        }
 
-        if (address_temp1=="null")
-        {
-            address_final=address_temp;
-        }
-        else{
-            address_final=address_temp1;
-        }
 
         Button order1 = findViewById(R.id.final_order);
         order1.setOnClickListener(new View.OnClickListener() {
@@ -162,25 +52,27 @@ public class address extends AppCompatActivity {
 
 
                     mDatabase.child("order").push().setValue(order1);
+                    //Database
+                    SQLiteDatabase myDB =
+                            openOrCreateDatabase("my.db", MODE_PRIVATE, null);
+                    myDB.execSQL(
+                            "CREATE TABLE IF NOT EXISTS user (name VARCHAR(200), total INT, address VARCHAR(200), mobile VARCHAR(200))"
+                    );
+                    ContentValues row1 = new ContentValues();
+                    row1.put("name", order_temp);
+                    row1.put("total", total_temp);
+                    row1.put("address", address_final);
+                    row1.put("mobile", mobile_final);
+                    myDB.insert("user", null, row1);
+
+                    myDB.close();
+
 
 
                 }
             });
 
-        //Database
-        SQLiteDatabase myDB =
-                openOrCreateDatabase("my.db", MODE_PRIVATE, null);
-        myDB.execSQL(
-                "CREATE TABLE IF NOT EXISTS user (name VARCHAR(200), total INT, address VARCHAR(200), mobile VARCHAR(200))"
-        );
-        ContentValues row1 = new ContentValues();
-        row1.put("name", order_temp);
-        row1.put("total", total_temp);
-        row1.put("address", address_final);
-        row1.put("mobile", mobile_final);
-        myDB.insert("user", null, row1);
 
-        myDB.close();
 
     }
 
