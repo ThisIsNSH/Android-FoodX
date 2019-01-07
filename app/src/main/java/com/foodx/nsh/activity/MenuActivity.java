d6e413a295944378b8007ad8061a37bf47ec5063
ThisIsNsh <hadanis.singh@gmail.com>
2019-01-07 12:07:40 +0530

    UI

--- app/src/main/java/com/foodx/nsh/activity/MenuActivity.java
+++ app/src/main/java/com/foodx/nsh/activity/MenuActivity.java
@@ -1,36 +1,17 @@
 package com.foodx.nsh.activity;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.LinearSnapHelper;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
-import android.view.ViewAnimationUtils;
-import android.view.ViewTreeObserver;
-import android.view.animation.AnticipateInterpolator;
-import android.view.animation.AnticipateOvershootInterpolator;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.foodx.nsh.R;
-import com.foodx.nsh.adapter.HotelAdapter;
 import com.foodx.nsh.adapter.MenuAdapter;
-import com.foodx.nsh.model.Hotel;
 import com.foodx.nsh.model.Item;
 import com.foodx.nsh.model.Menu;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -59,50 +40,84 @@ public class MenuActivity extends AppCompatActivity {
         menuAdapter = new MenuAdapter(bundle.getInt("color"),menuList, this);
         recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
         recyclerView.setAdapter(menuAdapter);
+        LinearSnapHelper snapHelper = new LinearSnapHelper() {
+            @Override
+            public int findTargetSnapPosition(RecyclerView.LayoutManager layoutManager, int velocityX, int velocityY) {
+                View centerView = findSnapView(layoutManager);
+                if (centerView == null)
+                    return RecyclerView.NO_POSITION;
+
+                int position = layoutManager.getPosition(centerView);
+                int targetPosition = -1;
+                if (layoutManager.canScrollHorizontally()) {
+                    if (velocityX < 0) {
+                        targetPosition = position - 1;
+                    } else {
+                        targetPosition = position + 1;
+                    }
+                }
+
+                if (layoutManager.canScrollVertically()) {
+                    if (velocityY < 0) {
+                        targetPosition = position - 1;
+                    } else {
+                        targetPosition = position + 1;
+                    }
+                }
+
+                final int firstItem = 0;
+                final int lastItem = layoutManager.getItemCount() - 1;
+                targetPosition = Math.min(lastItem, Math.max(targetPosition, firstItem));
+                return targetPosition;
+            }
+        };
+        snapHelper.attachToRecyclerView(recyclerView);
         getData();
     }
 
     public void getData() {
         List<Item> itemList = new ArrayList<>(),itemList1 = new ArrayList<>(),itemList2 = new ArrayList<>();
-        itemList.add(new Item("Item 1", "image", "100"));
-        itemList.add(new Item("Item 1", "image", "100"));
-        itemList.add(new Item("Item 1", "image", "100"));
+
+         itemList.add(new Item("Item 1", "image", "100"));
+         itemList.add(new Item("Item 1", "image", "100"));
+         itemList.add(new Item("Item 1", "image", "100"));
         itemList1.add(new Item("Item 1", "image", "100"));
         itemList1.add(new Item("Item 1", "image", "100"));
         itemList1.add(new Item("Item 1", "image", "100"));
         itemList2.add(new Item("Item 1", "image", "100"));
         itemList2.add(new Item("Item 1", "image", "100"));
         itemList2.add(new Item("Item 1", "image", "100"));
-        itemList.add(new Item("Item 1", "image", "100"));
-        itemList.add(new Item("Item 1", "image", "100"));
-        itemList.add(new Item("Item 1", "image", "100"));
+         itemList.add(new Item("Item 1", "image", "100"));
+         itemList.add(new Item("Item 1", "image", "100"));
+         itemList.add(new Item("Item 1", "image", "100"));
         itemList1.add(new Item("Item 1", "image", "100"));
         itemList1.add(new Item("Item 1", "image", "100"));
         itemList1.add(new Item("Item 1", "image", "100"));
         itemList2.add(new Item("Item 1", "image", "100"));
         itemList2.add(new Item("Item 1", "image", "100"));
         itemList2.add(new Item("Item 1", "image", "100"));
-        itemList.add(new Item("Item 1", "image", "100"));
-        itemList.add(new Item("Item 1", "image", "100"));
-        itemList.add(new Item("Item 1", "image", "100"));
+         itemList.add(new Item("Item 1", "image", "100"));
+         itemList.add(new Item("Item 1", "image", "100"));
+         itemList.add(new Item("Item 1", "image", "100"));
         itemList1.add(new Item("Item 1", "image", "100"));
         itemList1.add(new Item("Item 1", "image", "100"));
         itemList1.add(new Item("Item 1", "image", "100"));
         itemList2.add(new Item("Item 1", "image", "100"));
         itemList2.add(new Item("Item 1", "image", "100"));
         itemList2.add(new Item("Item 1", "image", "100"));
-        itemList.add(new Item("Item 1", "image", "100"));
-        itemList.add(new Item("Item 1", "image", "100"));
-        itemList.add(new Item("Item 1", "image", "100"));
+         itemList.add(new Item("Item 1", "image", "100"));
+         itemList.add(new Item("Item 1", "image", "100"));
+         itemList.add(new Item("Item 1", "image", "100"));
         itemList1.add(new Item("Item 1", "image", "100"));
         itemList1.add(new Item("Item 1", "image", "100"));
         itemList1.add(new Item("Item 1", "image", "100"));
         itemList2.add(new Item("Item 1", "image", "100"));
         itemList2.add(new Item("Item 1", "image", "100"));
         itemList2.add(new Item("Item 1", "image", "100"));
-        menuList.add(new Menu("Pizza",itemList));
-        menuList.add(new Menu("Pizza1",itemList1));
-        menuList.add(new Menu("Pizza2",itemList2));
-        menuAdapter.notifyDataSetChanged();
+         menuList.add(new Menu("Pizza",itemList));
+         menuList.add(new Menu("Pizza1",itemList1));
+         menuList.add(new Menu("Pizza2",itemList2));
+
+         menuAdapter.notifyDataSetChanged();
     }
 }
