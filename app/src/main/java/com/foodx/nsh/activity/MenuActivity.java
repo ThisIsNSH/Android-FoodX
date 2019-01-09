f9a0fb013818b73385d13008d02cb2ef22d4eea4
Abhinavlamba <abhinavlamba826@gmail.com>
2019-01-10 01:31:47 +0530

    DAY:1 Task Completed ;)

--- app/src/main/java/com/foodx/nsh/activity/MenuActivity.java
+++ app/src/main/java/com/foodx/nsh/activity/MenuActivity.java
@@ -5,16 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.LinearSnapHelper;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.foodx.nsh.R;
 import com.foodx.nsh.adapter.MenuAdapter;
 import com.foodx.nsh.model.Item;
 import com.foodx.nsh.model.Menu;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -22,6 +36,8 @@ public class MenuActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private MenuAdapter menuAdapter;
     private List<Menu> menuList;
+    String id;
+    JSONArray array;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +46,16 @@ public class MenuActivity extends AppCompatActivity {
         onInit();
     }
 
-    public void onInit(){
+    public void onInit() {
         hotelName = findViewById(R.id.hotel_name);
         recyclerView = findViewById(R.id.recyclerView);
         Bundle bundle = getIntent().getExtras();
 //        cardView.setCardBackgroundColor(getResources().getColor(bundle.getInt("color")));
         hotelName.setText(bundle.getString("name"));
+        id = bundle.getString("id");
         menuList = new ArrayList<>();
-        menuAdapter = new MenuAdapter(bundle.getInt("color"),menuList, this);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+        menuAdapter = new MenuAdapter(bundle.getInt("color"), menuList, this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         recyclerView.setAdapter(menuAdapter);
         LinearSnapHelper snapHelper = new LinearSnapHelper() {
             @Override
@@ -72,52 +89,63 @@ public class MenuActivity extends AppCompatActivity {
             }
         };
         snapHelper.attachToRecyclerView(recyclerView);
-        getData();
+        try {
+            getData();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void getData() {
-        List<Item> itemList = new ArrayList<>(),itemList1 = new ArrayList<>(),itemList2 = new ArrayList<>();
+    public void menulist() {
+        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest request = new StringRequest(Request.Method.GET, "https://fodo1.herokuapp.com/hotel/menu",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String jsonArray) {
+                        System.out.print(jsonArray);
 
-         itemList.add(new Item("Item 1", "image", "100"));
-         itemList.add(new Item("Item 1", "image", "100"));
-         itemList.add(new Item("Item 1", "image", "100"));
-        itemList1.add(new Item("Item 1", "image", "100"));
-        itemList1.add(new Item("Item 1", "image", "100"));
-        itemList1.add(new Item("Item 1", "image", "100"));
-        itemList2.add(new Item("Item 1", "image", "100"));
-        itemList2.add(new Item("Item 1", "image", "100"));
-        itemList2.add(new Item("Item 1", "image", "100"));
-         itemList.add(new Item("Item 1", "image", "100"));
-         itemList.add(new Item("Item 1", "image", "100"));
-         itemList.add(new Item("Item 1", "image", "100"));
-        itemList1.add(new Item("Item 1", "image", "100"));
-        itemList1.add(new Item("Item 1", "image", "100"));
-        itemList1.add(new Item("Item 1", "image", "100"));
-        itemList2.add(new Item("Item 1", "image", "100"));
-        itemList2.add(new Item("Item 1", "image", "100"));
-        itemList2.add(new Item("Item 1", "image", "100"));
-         itemList.add(new Item("Item 1", "image", "100"));
-         itemList.add(new Item("Item 1", "image", "100"));
-         itemList.add(new Item("Item 1", "image", "100"));
-        itemList1.add(new Item("Item 1", "image", "100"));
-        itemList1.add(new Item("Item 1", "image", "100"));
-        itemList1.add(new Item("Item 1", "image", "100"));
-        itemList2.add(new Item("Item 1", "image", "100"));
-        itemList2.add(new Item("Item 1", "image", "100"));
-        itemList2.add(new Item("Item 1", "image", "100"));
-         itemList.add(new Item("Item 1", "image", "100"));
-         itemList.add(new Item("Item 1", "image", "100"));
-         itemList.add(new Item("Item 1", "image", "100"));
-        itemList1.add(new Item("Item 1", "image", "100"));
-        itemList1.add(new Item("Item 1", "image", "100"));
-        itemList1.add(new Item("Item 1", "image", "100"));
-        itemList2.add(new Item("Item 1", "image", "100"));
-        itemList2.add(new Item("Item 1", "image", "100"));
-        itemList2.add(new Item("Item 1", "image", "100"));
-         menuList.add(new Menu("Pizza",itemList));
-         menuList.add(new Menu("Pizza1",itemList1));
-         menuList.add(new Menu("Pizza2",itemList2));
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+//                        Toast.makeText(MainActivity.this, "Unable to fetch data: " + volleyError.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> hashMap = new HashMap<>();
+                hashMap.put("hotel_id", id);
+                return hashMap;
+            }
+        };
+        requestQueue.add(request);
+    }
 
-         menuAdapter.notifyDataSetChanged();
+    public void getData() throws JSONException {
+//        menulist();
+        HashMap<String, ArrayList<Item>> menu = new HashMap<>();
+        array = new JSONArray("[{\"_id\":\"5c3607018469fa00170ce1cf\",\"category\":\"Category 1\",\"food_name\":\"Food 1\",\"food_price\":\"100\",\"food_image\":\"https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg\"},{\"_id\":\"5c3607018469fa00170ce1ce\",\"category\":\"Category 1\",\"food_name\":\"Food 2\",\"food_price\":\"100\",\"food_image\":\"https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg\"},{\"_id\":\"5c3607018469fa00170ce1cd\",\"category\":\"Category 1\",\"food_name\":\"Food 3\",\"food_price\":\"100\",\"food_image\":\"https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg\"},{\"_id\":\"5c3607018469fa00170ce1cc\",\"category\":\"Category 2\",\"food_name\":\"Food 3\",\"food_price\":\"100\",\"food_image\":\"https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg\"},{\"_id\":\"5c3607018469fa00170ce1cb\",\"category\":\"Category 2\",\"food_name\":\"Food 2\",\"food_price\":\"100\",\"food_image\":\"https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg\"},{\"_id\":\"5c3607018469fa00170ce1ca\",\"category\":\"Category 2\",\"food_name\":\"Food 1\",\"food_price\":\"100\",\"food_image\":\"https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg\"},{\"_id\":\"5c3607018469fa00170ce1c9\",\"category\":\"Category 3\",\"food_name\":\"Food 1\",\"food_price\":\"100\",\"food_image\":\"https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg\"},{\"_id\":\"5c3607018469fa00170ce1c8\",\"category\":\"Category 3\",\"food_name\":\"Food 2\",\"food_price\":\"100\",\"food_image\":\"https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg\"},{\"_id\":\"5c3607018469fa00170ce1c7\",\"category\":\"Category 3\",\"food_name\":\"Food 3\",\"food_price\":\"100\",\"food_image\":\"https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg\"}]");
+        for (int i = 0; i < array.length(); i++) {
+            Log.v("hello", String.valueOf(i));
+            JSONObject jsonObject = array.getJSONObject(i);
+            String category = jsonObject.getString("category");
+            if (menu.containsKey(category)) {
+                menu.get(category).add(new Item(jsonObject.getString("food_name"),
+                        jsonObject.getString("food_image"),
+                        jsonObject.getString("food_price")));
+            } else {
+                menu.put(category, new ArrayList<Item>());
+                menu.get(category).add(new Item(jsonObject.getString("food_name"),
+                        jsonObject.getString("food_image"),
+                        jsonObject.getString("food_price")));
+            }
+        }
+        for (Map.Entry<String, ArrayList<Item>> entry : menu.entrySet()) {
+            String h = entry.getKey();
+            ArrayList<Item> m = entry.getValue();
+            menuList.add(new Menu(h, m));
+        }
+        menuAdapter.notifyDataSetChanged();
     }
 }
