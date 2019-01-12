b772d51ae46efc27aa4a02520ba61bcf6fae9fb6
Abhinavlamba <abhinavlamba826@gmail.com>
2019-01-12 18:24:43 +0530

    Added:Login

--- app/src/main/java/com/foodx/nsh/activity/MenuActivity.java
+++ app/src/main/java/com/foodx/nsh/activity/MenuActivity.java
@@ -153,7 +153,6 @@ public class MenuActivity extends AppCompatActivity {
                 });
         requestQueue.add(request);
     }
-
     public void getData() throws JSONException {
         menulist();
     }
