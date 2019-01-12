9d5574171bce1a6e78750df9944d1c31daf2d5d5
ThisIsNsh <hadanis.singh@gmail.com>
2019-01-12 11:20:37 +0530

    old changes

--- app/src/main/java/com/foodx/nsh/activity/MainActivity.java
+++ app/src/main/java/com/foodx/nsh/activity/MainActivity.java
@@ -4,35 +4,17 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.content.res.ResourcesCompat;
-import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.Window;
-import android.view.WindowManager;
 
 import com.foodx.nsh.R;
 import com.foodx.nsh.fragments.CartFragment;
 import com.foodx.nsh.fragments.HotelsFragment;
-import com.foodx.nsh.model.Hotel;
-import com.foodx.nsh.model.Item;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.w3c.dom.Text;
-
-import java.util.ArrayList;
-import java.util.List;
-import com.foodx.nsh.fragments.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,12 +25,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Window w = getWindow();
-            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        }
         btmView = findViewById(R.id.nav_bar);
         btmView.setOnNavigationItemSelectedListener(navigationItemReselectedListener);
+        btmView.setItemBackgroundResource(R.drawable.transparent);
+
         Intent i = getIntent();
         String data = i.getStringExtra("FromReservation");
         if (data != null && data.contentEquals("1")) {
@@ -57,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
             fragmentTransaction.commitNow();
         }
         else
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HotelsFragment()).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HotelsFragment(MainActivity.this)).commit();
 
         int[] colors = new int[] {
                 Color.parseColor("#afb42b"),
@@ -69,10 +49,6 @@ public class MainActivity extends AppCompatActivity {
                 new int[] {android.R.attr.state_enabled, android.R.attr.state_checked}
         };
         btmView.setItemTextColor(new ColorStateList(states, colors));
-//       btmView.setBackground(ActivityCompat.getDrawable(getApplicationContext(),R.drawable.white_gradient));
-
-//        onInit();
-       // getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HotelsFragment(MainActivity.this)).commit();
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navigationItemReselectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -82,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
             Fragment fragment = null;
 
             int[] colors = new int[] {
-                      Color.parseColor("#afb42b"),
+                    Color.parseColor("#afb42b"),
                     Color.parseColor("#9e9d24")
             };
 
@@ -95,23 +71,15 @@ public class MainActivity extends AppCompatActivity {
             {
                 case R.id.hotels:
                     btmView.setItemTextColor(new ColorStateList(states, colors));
-//                    btmView.setItemIconTintList(new ColorStateList(states, colors));
-                    fragment = new HotelsFragment();
-                    break;
-                case R.id.carts:
-                    btmView.setItemTextColor(new ColorStateList(states,colors));
-                    fragment = new CartFragment();
-                    break;
-                case R.id.settings:
-                    btmView.setItemTextColor(new ColorStateList(states,colors));
-                    fragment = new Fragment();
                     fragment = new HotelsFragment(MainActivity.this);
                     break;
                 case R.id.carts:
+                    btmView.setItemTextColor(new ColorStateList(states,colors));
                     fragment = new CartFragment(MainActivity.this);
                     break;
                 case R.id.settings:
-                    fragment = new SettingsFragment(MainActivity.this);
+                    btmView.setItemTextColor(new ColorStateList(states,colors));
+                    fragment = new HotelsFragment(MainActivity.this);
                     break;
             }
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
@@ -119,51 +87,4 @@ public class MainActivity extends AppCompatActivity {
         }
 
     };
-            }
-        });
-        hotelList = new ArrayList<>();
-        hotelAdapter = new HotelAdapter(hotelList, this);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(hotelAdapter);
-        getData();
-    }
-    public void getData() {
-        final RequestQueue requestQueue = Volley.newRequestQueue(this);
-            JsonArrayRequest request = new JsonArrayRequest("https://fodo1.herokuapp.com/hotel",
-                    new Response.Listener<JSONArray>() {
-                        @Override
-                        public void onResponse(JSONArray jsonArray) {
-                            for(int i = 0; i < jsonArray.length(); i++) {
-                                try {
-                                    JSONObject jsonObject = jsonArray.getJSONObject(i);
-//                                    mEntries.add(jsonObject.toString());
-                                    String name, location, mobile, id;
-                                id = jsonObject.getString("_id");
-                                name = jsonObject.getString("name");
-                                location = jsonObject.getString("location");
-                                mobile = jsonObject.getString("mobile");
-                                hotelList.add(new Hotel(id, name, "https://i.dlpng.com/static/png/151888_preview.png", mobile, location));
-                                }
-                                catch(JSONException e) {
-                                }
-                            }
-                            hotelAdapter.notifyDataSetChanged();
-                        }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError volleyError) {
-                            Toast.makeText(MainActivity.this, "Unable to fetch data: " + volleyError.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    });
-            requestQueue.add(request);
-    }
-//    public void setBackground(Drawable background) {
-//        //noinspection deprecation
-//        setBackgroundDrawable(background);
-//    }
-//
-//    @Deprecated
-//    public void setBackgroundDrawable(Drawable background) {}
-=======
 }
